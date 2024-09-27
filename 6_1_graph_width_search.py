8913af72701cb073cf376203e8485f602bb4dbe9
srt-2000 <srt2000888@gmail.ru>
2024-09-27 20:07:13 +0300

    OOP code tuning

--- 6_1_graph_width_search.py
+++ 6_1_graph_width_search.py
@@ -1,34 +1,39 @@
 from collections import deque
 
-graph = {}
-graph["you"] = ["Vova", "Max", "Demon"]
-graph["Vova"] = ["Leo", "Alex"]
-graph["Max"] = ["Denis", "Dima"]
-graph["Demon"] = ["Ann"]
-graph["Leo"] = []
-graph["Alex"] = []
-graph["Denis"] = ["Andrey"]
-graph["Dima"] = []
-graph["Ann"] = []
-graph["Andrey"] = []
+class MSearcher:
 
-def mango_seller(name):
-    return name[-1] == "m"
+    def __init__(self):
+        self.person = None
+        self.searched = None
+        self.search_queue = None
+        self.name = None
+        self.graph_ = None
 
-def search_mango_seller(name):
-    search_queue = deque()
-    search_queue += graph[name]
-    searched = []
-    while search_queue:
-        person = search_queue.popleft()
-        if person not in searched:
-            if mango_seller(person):
-                print(person, "is mango seller")
-                return True
-            else:
-                searched.append(person)
-                search_queue += graph[person]
-    print("There are not mango-sellers in my friends circle")
-    return False
+    def search_mango_seller(self, name, graph_):
+        self.graph_ = graph_
+        self.name = name
+        self.search_queue = deque()
+        self.search_queue += self.graph_[self.name]
+        self.searched = []
+        while self.search_queue:
+            self.person = self.search_queue.popleft()
+            if self.person not in self.searched:
+                if self.person[-1] == "m":
+                    print(self.person, "is mango seller")
+                    print("I've already checked this friends:\n", self.searched)
+                    return True
+                else:
+                    self.searched.append(self.person)
+                    self.search_queue += graph_[self.person]
+        print("I've already checked this friends:\n", self.searched)
+        print(f"There are not mango-sellers in {self.name}s friends circle")
+        return False
 
-search_mango_seller("you")
+graph = {"you": ["Viva", "Max", "Demon"], "Viva": ["Leo", "Alex"], "Max": ["Denis", "Dima"], "Demon": ["Ann"],
+         "Leo": [], "Alex": [], "Denis": ["Andrey-m"], "Dima": [], "Ann": [], "Andrey": []}
+
+print("Hello!\nI'm a finder if a mango-seller (name has 'm' at tne end) in friends circle.\nPlease, have a look on this graph.\n")
+print(graph, "\n\nAnd write the name you want to check.")
+a = str(input("Name:"))
+b = MSearcher()
+b.search_mango_seller(a, graph)
--- 7_1_graph_Deikstr.py
+++ 7_1_graph_Deikstr.py
@@ -2,14 +2,11 @@ graph = {}
 graph["start"] = {}
 graph["start"]["a"] = 6
 graph["start"]["b"] = 2
-
 graph["a"] = {}
 graph["a"]["fin"] = 1
-
 graph["b"] = {}
 graph["b"]["a"] = 3
 graph["b"]["fin"] = 5
-
 graph["fin"] = {}
 # ====end graph======
 
